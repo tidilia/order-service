aa4109d59a2d7d56cee99500def23bd40b057a86
tidilia <tidiliaaa@gmail.com>
2026-05-23 21:14:47 +0500

    db url in config changed

--- app/config.py
+++ app/config.py
@@ -5,3 +5,5 @@ LMS_API_KEY = os.getenv("LMS_API_KEY")
 CAPASHINO_URL = os.getenv("CAPASHINO_URL")
 SENTRY_DSN = os.getenv("SENTRY_DSN")
 DATABASE_URL = os.getenv("POSTGRES_CONNECTION_STRING")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
